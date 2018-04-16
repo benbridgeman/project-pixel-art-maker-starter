7e379097616112af457c84ef3442744039cadd96
Ben Bridgeman <ben_bridgeman@hotmail.co.uk>
2018-04-16 16:26:35 +0100

    Write basic functionality for makeGrid

--- designs.js
+++ designs.js
@@ -11,5 +11,15 @@ const table = document.getElementById('pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
-
+  // Add rows
+  for (let row = 0; row < gridHeight.value; row++) {
+    const gridTr = document.createElement('tr');
+    table.appendChild(gridTr);
+    //Add columns
+    for (let col = 0; col < gridWidth.value; col++) {
+      const gridTd = document.createElement('td');
+      gridTr.appendChild(gridTd);
+    }
+  }
+  console.log('Grid Dimensions:\n- Grid height = ' + gridHeight.value + '\n- Grid width = ' + gridWidth.value);
 };
