185f0c49723a0b3a665e9788ad1cc0db988276df
Ben Bridgeman <ben_bridgeman@hotmail.co.uk>
2018-04-16 16:25:21 +0100

    Declare variables for the project

--- designs.js
+++ designs.js
@@ -1,10 +1,15 @@
 // Select color input
+const color = document.getElementById('colorPicker');
+
 // Select size input
+const size = document.getElementById('sizePicker');
+const gridHeight = document.getElementById('inputHeight');
+const gridWidth = document.getElementById('inputWeight');
+
+// Select grid table
+const table = document.getElementById('pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
 
-// Your code goes here!
-
-}
+};
